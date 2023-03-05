29afbbaeeefacb83898febe7863e35dc2d3c31ba
nitesh <nitesh9501@hotmail.com>
2023-03-05 15:02:26 +0530

    Minor change

--- controllers/entity.js
+++ controllers/entity.js
@@ -1,10 +1,12 @@
 import User from "../models/user.js";
+import Entity from "../models/entity.js";
 
 export default {
   async getEntities(req, res) {
     try {
-      const user = await User.findOne({ _id: res.locals.user.id }).populate([ "entityModels" ]);
-      res.status(200).json(user.entityModels);
+      const user = await User.findOne({ _id: res.locals.user.id });
+      const entities = await Entity.find({ _id: { $in: user.entities } }, { creator: 0 });
+      res.status(200).json(entities);
     } catch (e) {
       res.status(500).json({});
     }
